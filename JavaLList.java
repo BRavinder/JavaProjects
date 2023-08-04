8521fa9ea922975644983ddb7c3a94ee51e5c37c
BRavinder <105974685+BRavinder@users.noreply.github.com>
2023-08-04 11:00:44 +0530

    Update JavaLList.java
    update

--- JavaLList.java
+++ JavaLList.java
@@ -1,41 +1,52 @@
 import java.util.*;
-class JavaLList{
+import java.util.LinkedList;
+
+class JavaLList {
     public static void main(String[] args) {
         LinkedList<Integer> l1 = new LinkedList<>();
-        //Add elements in to the LList
+
+        // Add elements into the LinkedList
         l1.add(2);
         l1.add(8);
         l1.add(7);
         l1.add(5);
-        //Adding index number to a element
-        l1.add(2,1);
-        System.out.print(l1);
 
-        //remove elements from the list
-        System.out.println();
-            l1.remove(2);
-            System.out.print(l1);
-         //remove first element
-        System.out.println();
-            l1.removeFirst();
-        System.out.print(l1);
+        // Adding an element with value 1 at index 2
+        l1.add(2, 1);
+        System.out.print(l1); // Output: [2, 8, 1, 7, 5]
 
-            //remove last element
-            System.out.println();
-                l1.removeLast();
-            System.out.print(l1);
-            // Print for the line
-            System.out.println();
-           // Iterating over the elements  
-        Iterator it = l1.iterator();
-        while(it.hasNext()){
-            System.out.print(it.next()+ " ");
+        // Remove an element at index 2
+        l1.remove(2);
+        System.out.print(l1); // Output: [2, 8, 7, 5]
+
+        // Remove the first element
+        l1.removeFirst();
+        System.out.print(l1); // Output: [8, 7, 5]
+
+        // Remove the last element
+        l1.removeLast();
+        System.out.print(l1); // Output: [8, 7]
+
+        // Print a new line
+        System.out.println();
+
+        // Iterating over the elements using an iterator
+        Iterator<Integer> it = l1.iterator();
+        while (it.hasNext()) {
+            System.out.print(it.next() + " "); // Output: 8 7
         }
-        System.out.println();
-        System.out.print(l1.size());
-        System.out.println();
-        boolean b1 = l1.contains(11);
-        System.out.println(b1);
 
+        // Print a new line
+        System.out.println();
+
+        // Get the size of the LinkedList
+        System.out.print(l1.size()); // Output: 2
+
+        // Print a new line
+        System.out.println();
+
+        // Check if the LinkedList contains the element 11
+        boolean b1 = l1.contains(11);
+        System.out.println(b1); // Output: false
     }
 }
